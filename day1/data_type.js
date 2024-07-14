ab8d3f15efda64e6e01426b121a9e4038d180fc0
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-07-14 22:07:37 +0545

    Introduction to js all

--- day1/data_type.js
+++ day1/data_type.js
@@ -36,3 +36,54 @@ let i = true;
 let j = false;
 
 console.log(typeof i, typeof j)
+
+
+// symbol
+// we have to make unique key that time we use symbol
+// to store key of object 
+
+
+// let sym = symbol()
+// console.log(sym)
+
+
+// derived data type
+// combined data type
+
+let o = null; // null , 0, false, ""
+
+let p // undefined
+
+console.log(typeof o, typeof p)
+
+
+
+//array and object
+
+let k = []
+console.log(typeof k) // object  // array is object in js
+
+let l = new Array(1, 2, 3, 4, 5) // array as constructor
+
+console.log(l)
+
+// to make json object we use {} curly braces
+
+let m = {} // object  called as prototype object
+
+console.log(typeof m) // is string then it is Json 
+    // if object then it is object
+
+// array = index based 
+// object = key value pair and object  are known as JSON object, 
+// json is universal data type
+// json is used to  store data in key value pair
+// json data is readable in all the languages
+
+// function
+
+// functoin in js data type is function
+
+let n = function() {
+    // function as a data type
+}
