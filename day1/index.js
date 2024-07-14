2aaedea4e342fadc9195f9d285e6a82701da9522
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-07-14 21:19:25 +0545

    scope of variabel block scope

--- day1/index.js
+++ day1/index.js
@@ -1,3 +1,25 @@
 let Name = "Manmohan"
 
 console.log(Name)
+
+
+var email = "manmohan@gmial.com"
+console.log(email)
+
+
+// scope of variables
+
+
+// block scope
+
+let x = 12;
+
+console.log(x)
+
+
+
+
+{
+    let x = 13;
+    console.log(x)
+}
