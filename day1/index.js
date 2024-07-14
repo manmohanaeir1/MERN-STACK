26579bf97aab046f03f33e149644338c0c9e4eac
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-07-14 21:32:12 +0545

    const

--- day1/index.js
+++ day1/index.js
@@ -40,3 +40,22 @@ console.log(y) // 3
 
 // var dosent take block scope
 // just update value in Var not redecleration
+
+
+
+
+//..... constant 
+
+const abc = 30;
+console.log(abc)
+
+// abc = 40; // error
+
+
+let a = 34;
+a = "manmohan" //string so, js is losely coupled language
+
+
+let fname; // unless you assign value to variable it will not create memory space
+
+// thats why we called js is dynamically typed losely coupled programming language
