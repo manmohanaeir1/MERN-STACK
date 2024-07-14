0d10513136bb33c79785f41e525ca91fa9f55453
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-07-14 21:23:49 +0545

    scope of variabel block scope in Var

--- day1/index.js
+++ day1/index.js
@@ -23,3 +23,20 @@ console.log(x)
     let x = 13;
     console.log(x)
 }
+
+
+// for var 
+
+var y = 1;
+console.log(y)
+
+{
+    var y = 3;
+    console.log(y)
+
+}
+
+console.log(y) // 3
+
+// var dosent take block scope
+// just update value in Var not redecleration
