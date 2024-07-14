5ebe064b858f9543af9fba8f53ac3bf774837b51
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-07-14 21:44:11 +0545

    string

---
+++ day1/data_type.js
@@ -0,0 +1,21 @@
+// data types in js
+
+
+// premetive (original ) number string boolean and symbol
+
+// derived (copied) object array function
+
+
+
+let a = "single " // string  // single line 
+let b = 'single' // string // single line 
+let c = ` multi line` // string   //  multi line 
+
+let d = `
+    <h1> hello </h1>
+    ${a}
+    `
+console.log(d)
+
+
+console.log(typeof a, typeof b, typeof c)
