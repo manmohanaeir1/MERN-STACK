fc4f34a8cb31b51b188d045ff1ed18d6ec29bd34
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-07-15 22:38:05 +0545

    Array inserting method\

--- day2/array-oop.js
+++ day2/array-oop.js
@@ -10,6 +10,8 @@ const userOb = {
 }
 
 
+console.log(allUser)
+
 // INSERT
 
 //push operation  => default  operation insert at last (Quee new entry in last and read first)
@@ -18,13 +20,20 @@ const userOb = {
 
 allUser.push(userOb) // first element is inserted in 0 index
 
+console.log(allUser)
+
 
 // top insert
 allUser.unshift(userOb) //0 indexdata entryexistng data is shifted to next index
 
+console.log(allUser)
+
 // any index
 
 allUser[3] = userOb
+
+console.log(allUser)
+
 
 // [ 0 => {}, 1 => {}, <empty slot x 1 >, 3 => {}]
 
@@ -46,3 +55,20 @@ allUser[size] = userOb;
 // for that we have splice method
 
 allUser.splice(1, 1, userOb) // 1 index is removed and new data is inserted at 1 index
+
+
+//remove data
+//PUSH 
+// take last index vlue and remove it
+
+let lastItem = allUser.pop() // last index data is removed and stored in lastItem
+
+//unshift 
+
+let firstItem = allUser.shift()
+
+//take middle value
+
+let index = allUser.splice(1, 2) // 1 index data is removed and stored in index
+
+console.log(allUser)
