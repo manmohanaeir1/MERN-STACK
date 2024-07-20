d9921688550ec1eb15032d9e3af0221b62e3c195
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-07-20 22:14:00 +0545

    operatoer in js

---
+++ day3/operators.js
@@ -0,0 +1,23 @@
+/**
+ *
+ **/
+
+let a = 10;
+let b = 20;
+const c = a + b;
+
+
+console.log(c); // 30
+
+
+const d = a / b;
+console.log(d); // 0.5
+
+const e = a % b;
+console.log(e); //
+
+a = a + 1; //11
+a = a++; //12
+a = ++a; //13
+console.log(a++); // 13   post assignment
+console.log(++a); // 15   pre assignment
