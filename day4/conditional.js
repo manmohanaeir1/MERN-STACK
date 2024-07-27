e68063f83216a7f8259c9b81716a4f63854012dc
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-07-27 22:18:17 +0545

    conditional statment if else D

---
+++ day4/conditional.js
@@ -0,0 +1,43 @@
+let age = 23;
+
+if (age >= 18) {
+    console.log("You are an adult");
+}
+
+// The if statement is used to execute a block of code if a condition is true.
+// The if statement is used to execute a block of code if a condition is true.
+
+let mode = "light";
+let color = ""
+
+if (mode === "dark") {
+    color = "black";
+}
+
+if (mode === "light") {
+    color = "white";
+}
+console.log(color); // black
+
+
+
+// if else 
+
+let mode1 = "light";
+let color1 = ""
+
+if (mode1 === "light") {
+    color1 = "white";
+} else {
+    color1 = "black";
+}
+console.log(color1); // white
+
+
+
+
+if (age >= 18) {
+    console.log("you are eligible for voting");
+} else {
+    console.log("you are not eligible for voting");
+}
