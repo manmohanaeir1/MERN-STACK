45ddffe5cf8fcf90897bf65b1160fb5534b7b559
Manmohan Aeir <manmohanaeir001110@gmail.com>
2024-08-04 21:37:22 +0545

    function in js - build in function

---
+++ day8/function.js
@@ -0,0 +1,9 @@
+// function , methods, events , task all are the same thing
+// function is a block of code that can be called by name. The code inside a function is not executed when the function is defined. It is executed when the function is invoked.
+//
+
+// build in function
+
+const name = 'MANMOHAN';
+const lowerCaseName = name.toLowerCase();
+console.log(lowerCaseName);
